8b6e92093b4e87d654c451b6129039807e3ebc09
Rafailong <rafa.avim@gmail.com>
2017-10-03 23:57:35 -0500

    Add validation styling and submit button disabled depending in form status.

--- Scripts/app/login.component.ts
+++ Scripts/app/login.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, FormControl, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 
 @Component({
@@ -10,32 +10,47 @@ import {Router} from "@angular/router";
     <div class="alert alert-danger" role="alert" *ngIf="loginError">
       <strong>Warning:</strong> Username or Password mismatch
     </div>
-    <form class="form-login" [formGroup]="loginForm" (submit)="performLogin($event)" novalidate >
-      <input type="text" class="form-control" formControlName="username"
-        placeholder="Your username or e-mail address" required autofocus />
-      <input type="password" class="form-control" formControlName="password"
-        placeholder="Your password" required />
-      <div class="checkbox">
-        <label>
-          <input type="checkbox" value="remember-me" />
-          Remember me
-        </label>
+    <form class="form-login" [formGroup]="loginForm" (submit)="performLogin($event)" >
+
+      <div class="form-group"
+        [ngClass]="{
+          'has-error': username.invalid && (username.dirty || username.touched),
+          'has-success': username.valid && (username.dirty || username.touched)
+        }">
+        <label for="username">eMail</label>
+        <input type="text" class="form-control" formControlName="username"
+          placeholder="Your username or e-mail address" required />
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+
+      <div class="form-group"
+        [ngClass]="{
+          'has-error': password.invalid && (password.dirty || password.touched),
+          'has-success': password.valid && (password.dirty || password.touched)
+        }">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" formControlName="password" name="password"
+          required />
+      </div>
+
+      <button class="btn btn-lg btn-primary btn-block" type="submit" [disabled]="!this.loginForm.valid">Sign in</button>
     </form>
-    <p>Form status: {{ loginForm.status | json }}</p>
   </div>
   `
 })
 export class LoginComponent {
   title = "Login";
   loginForm: FormGroup;
+  username: FormControl;
+  password: FormControl;
+
   loginError: any = null;
 
   constructor(private fb: FormBuilder) {
+    this.username = new FormControl('', Validators.required);
+    this.password = new FormControl('', Validators.required);
     this.loginForm = fb.group({
-      username: ["", Validators.required],
-      password: ["", Validators.required]
+      username: this.username,
+      password: this.password
     });
   }
   performLogin(e: any) {
